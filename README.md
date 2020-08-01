9d83e4ee3cd20d94bff00362def2fb44a75f1f22
Nitin Prakash <prakash.nitin63@gmail.com>
2020-08-01 16:50:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# learn-haskell
+
+
+This is an attempt to get familiar with haskell
+
+### References
+
+1. Learnyouhaskell
+2. H-99
---
+++ src/List.hs
@@ -0,0 +1,70 @@
+import Data.List (group)
+
+{-
+ - Problem 1: Find the last element of a list.
+ -}
+
+lastElement :: [a] -> a
+lastElement [] = error "List is empty Jackass!"
+lastElement [x] = x
+lastElement (x : xs) = lastElement xs
+
+
+{-
+ - Problem 2: Find the last but one element of a list
+ -}
+lastButOneElement :: [a] -> a
+lastButOneElement [] = error "List is empty Jackass"
+lastButOneElement [x] = error "List has only one element"
+lastButOneElement [x, _] = x
+lastButOneElement (_ : xs) = lastButOneElement xs
+
+
+{-
+ - Problem 3: Find the K'th element of a list. The first element in the list is number 1.
+ -}
+kthElement :: [a] -> Int -> a
+kthElement [] k = error "List is empty Jackass"
+kthElement xs k = xs !! (k - 1)
+
+
+{-
+ - Problem 4: Find the number of elements of a list.
+ -}
+numOfElements :: [a] -> Int
+numOfElements [] = error "List is empty Jackass"
+numOfElements xs = sum [1 | _ <- xs]
+
+
+{-
+ - Problem 5: Reverse a list.
+ -}
+reverseList :: [a] -> [a]
+reverseList [] = []
+reverseList (x : xs) = reverseList xs ++ [x]
+
+
+{-
+ - Problem 6: Find out whether a list is a palindrome.
+ - A palindrome can be read forward or backward; e.g. (x a m a x).
+ -}
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome xs = xs == (reverse xs)
+
+
+{-
+ - Problem 7: Flatten a nested list structure.
+ -}
+data NestedList a = Elem a | List [NestedList a] deriving (Show, Eq)
+flattenList :: NestedList a -> [a]
+flattenList (Elem x) = [x]
+flattenList (List x) = concatMap flattenList x
+
+
+{-
+ - Problem 8: Eliminate consecutive duplicates of list elements.
+ -}
+compress :: Eq a => [a] -> [a]
+compress = map head . group
